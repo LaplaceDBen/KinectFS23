0f78a85c03fa5ffcfb9403bc5818328db168e192
LaplaceDBen <77884145+LaplaceDBen@users.noreply.github.com>
2023-03-18 20:54:31 +0100

    added depth search of closest object

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from detection_func import *
 from visual_func import *
 
-#devices()
 
+#devices()
+'''
 df,image = detect_objects("images/rectangles.png", 0.7,0.95)
 print(df.head())
 cv2.imshow("Objects", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+'''
+live_depth()
