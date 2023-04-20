81a6dd4768aebbe59506e82dc87f29c17fd24f67
LaplaceDBen <benitomichelerusconi@outlook.com>
2023-04-20 16:34:36 +0200

    added some detector tests

--- Test_scripts/visual_test.py
+++ Test_scripts/visual_test.py
@@ -1,76 +1,52 @@
-import pyvista as pv
-from pyvistaqt import BackgroundPlotter
+import pygame
+from OpenGL.GL import *
+from OpenGL.GLU import *
 
-# Function for updating the positions of the meshes and light in the update loop as a callback
-def update_scene():
-    # PyVista has simplified rotation methods for each rotation direction or you call call the rotate method and explicitly specify all rotations
-    # We call inplace=True to update the changes to the mesh directly
-    #mesh.rotate_y(10,inplace=True)
-    sphere.rotate_y(-10,inplace=True)
-    #move mesh 10 cells in x axis
-    mesh1.translate([10,0,0],inplace=False)
-    #print position of mesh
-    #print(mesh.center)
-    #move mesh to position
+def draw_grass():
+    glBegin(GL_QUADS)
+    glColor3f(0.0, 1.0, 0.0)
+    glVertex3f(-1.0, 0.0, -1.0)
+    glVertex3f(-1.0, 0.0, 1.0)
+    glVertex3f(1.0, 0.0, 1.0)
+    glVertex3f(1.0, 0.0, -1.0)
+    glEnd()
 
-    
-    mesh1.rotate_y(-10,inplace=False)
-    
-    
-    # As the light object does not contain a rotate method, we just update its position based on the sphere.center position. 
-    # The position method can both get and set the light position.
-    light.position = sphere.center
-    # We update the whole plotter
-    p.update()
+def draw_house(x, y):
+    glPushMatrix()
+    glTranslatef(x, 0.0, y)
+    glBegin(GL_QUADS)
+    glColor3f(1.0, 0.0, 0.0)
+    glVertex3f(-0.5, 0.0, -0.5)
+    glVertex3f(-0.5, 0.5, -0.5)
+    glVertex3f(0.5, 0.5, -0.5)
+    glVertex3f(0.5, 0.0, -0.5)
+    glEnd()
+    glPopMatrix()
 
-if __name__ == '__main__':
-    # Load the mesh or point cloud if required
-    mesh_path = 'Test_scripts\model.obj'
-    mesh = pv.read(mesh_path)
-    mesh1 = pv.read(mesh_path)
-    # if the mesh has a texture, it needs to be loaded separetely using read_texture
-    #tex_path = 'mesh/angelStatue_lp.jpg'
-    #tex = pv.read_texture(tex_path)
+def main():
+    pygame.init()
+    display = (800, 600)
+    pygame.display.set_mode(display, pygame.DOUBLEBUF | pygame.OPENGL)
 
-    # We create a plotter for non-blocking visualization, using pyvistaqt. If we don't need interactivity or animations we can just call pyvista.Plotter
-    p = BackgroundPlotter(window_size=(1280, 720), title='PyVistaQt Plotter', multi_samples=8, line_smoothing=True, point_smoothing=True, polygon_smoothing=True, auto_update=False)
-    
-    # We set the camera position to be in the xy plane 
-    p.camera_position = [(0, 0, 100), (0, 0, 0), (0, 1, 0)]
-    # We set the front and back clipping planes
-    p.camera.clipping_range = (0, 100)
+    gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
+    glTranslatef(0.0, -0.5, -5)
 
-    # We create the sphere which will orbit the angel statue and set its radius and center
-    sphere = pv.Sphere(radius = 0.2, center=(0, 0, 100))
+    house_positions = [(0.0, 0.0), (1.0, 1.0)]
 
-    # We create a blue point light at the position of the sphere
-    light = pv.Light((0, 0, 1), (0, 0, 0), 'blue')
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
 
-    # We add the angel statue mesh together with its texture to the plotter
-    p.add_mesh(mesh,name='angel',texture = None)
-    p.add_mesh(mesh1, color='red', opacity=0.5, name='angel1', show_edges=True, line_width=5, render_points_as_spheres=True)
-    # We add the sphere to the plotter. Just to demonstrate that PyVista supports the pbr rendering from VTK we make the sphere metallic.
-    # If you get an error here your VTK version is <9.0. Either remove the pbr value or upgrade.
-    p.add_mesh(sphere, pbr=True, metallic=0.7, roughness=0.2, diffuse=1)
-    # We add the light to the plotter
-    p.add_light(light)
-    p.add_axes()
-    p.add_bounding_box()
-    #m0ve mesh1 to diffrent side of bounding box
-    mesh1.translate([10,0,0],inplace=False)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
-    # We add the update_scene function as a callback to the plotter. The interval is in milliseconds.
-    p.add_callback(update_scene, interval=100)
-    
-    p.add_toolbars()
-    p.add_slider_widget(update_scene, [0, 100], value=50, title='Slider', pointa=(0.8, 0.1), pointb=(0.9, 0.1))
+        draw_grass()
 
+        for i, (x, y) in enumerate(house_positions):
+            draw_house(x, y)
 
-    #p.add_callback(update_scene, interval=100)
-    print(mesh)
+        pygame.display.flip()
 
-    # We show the plotter and call p.app.exec_(), so the plotter stays open. This is important when running the pyvistaqt plotter.
-    # If your visualization automatically closes add the last line
-    p.show() 
-    p.app.exec_()
-
+if __name__ == "__main__":
+    main()
--- detection_func.py
+++ detection_func.py
@@ -6,6 +6,8 @@ from pyk4a import Config, PyK4A
 import time
 from datetime import datetime
 from config import camera_config
+from numba import jit
+import logging
 
 class QRCodeDetector:
     
@@ -79,10 +81,11 @@ class QRCodeDetector:
 
                         # Write terminal output to log file
                         log_file.write(f"{qr_codes[i]} ({coords[i][0]}, {coords[i][1]}) Angle: {round(angles[i], 2)} ")
+                        
 
                     
                     # Write timestamp to log file
-                log_file.write(f"({datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]})\n")
+                #og_file.write(f"({datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]})\n")
                 log_file.flush()
 
                 print("\nNext Search: ", datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3])
@@ -90,7 +93,7 @@ class QRCodeDetector:
                 cv2.imshow("QR Codes", color_image)
                 cv2.resizeWindow("QR Codes", (color_image.shape[1], color_image.shape[0]))
 
-            key = cv2.waitKey(1)
+            key = cv2.waitKey(1) #wait for 1ms the loop will start again and we will process the next frame
             if key == 27 or key == 127: # Quit on Esc or Delete key
                 k4a.stop()
                 log_file.close()
@@ -98,7 +101,120 @@ class QRCodeDetector:
 
         k4a.stop()
         log_file.close()
-       
+
+
+
+class QRCodeDetector_2:
+    
+    @staticmethod
+    def detect_qr_codes(num_obj=5):
+        k4a = camera_config
+        k4a.start()
+
+        cv2.namedWindow("QR Codes", cv2.WINDOW_NORMAL)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        font_scale = 1
+
+        # Create logger and log file
+        logger = logging.getLogger("QR Code Detection Log")
+        logger.setLevel(logging.DEBUG)
+        log_format = logging.Formatter('%(asctime)s - %(message)s')
+        file_handler = logging.FileHandler('logfile.log')
+        file_handler.setFormatter(log_format)
+        logger.addHandler(file_handler)
+
+        logger.info(f"QR Code Detection Log ({time.strftime('%Y-%m-%d %H:%M:%S')})\n\n")
+        
+        while True:
+            frame = k4a.get_capture()
+            if frame is None:
+                continue
+
+            color_image = frame.color
+            decoded_objects = decode(color_image)
+
+            if len(decoded_objects) != num_obj:
+                continue
+
+            qr_codes, coords, angles = [], [], []
+            for obj in decoded_objects:
+                qr_code = obj.data.decode("utf-8")
+                qr_codes.append(qr_code)
+
+                coord = (int(obj.rect.left + obj.rect.width/2), int(obj.rect.top + obj.rect.height/2))
+                coords.append(coord)
+
+                rect = cv2.minAreaRect(np.array(obj.polygon, np.int32))
+                angle = rect[2]
+                angles.append(angle)
+
+                cv2.polylines(
+                    color_image,
+                    [np.array(obj.polygon, np.int32).reshape((-1, 1, 2))],
+                    True,(0, 255, 0),2,)
+                
+                cv2.putText(
+                    color_image,qr_code,(obj.rect.left, obj.rect.top - 30),
+                    font,font_scale,(0, 255, 0),2,)
+                
+                cv2.putText(
+                    color_image,f"({coord[0]}, {coord[1]})",(coord[0], coord[1] - 10),font,
+                    font_scale,(0, 255, 0),2,)
+
+                logger.info(f"{qr_code} ({coord[0]}, {coord[1]}) Angle: {round(angle, 2)} ")
+                print(f"{qr_code} Angle: {round(angle, 2)} ({coord[0]}, {coord[1]})")
+
+            logger.handlers[0].flush()
+            print("\nNext Search: ", datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3])
+
+            cv2.imshow("QR Codes", color_image)
+            cv2.resizeWindow("QR Codes", (color_image.shape[1], color_image.shape[0]))
+
+            key = cv2.waitKey(1)
+            if key in [27, 127]:
+                k4a.stop()
+                logger.handlers[0].close()
+                break
+
+        k4a.stop()
+        logger.handlers[0].close()
+
+
+class QR_Detector_3:
+    def __init__(self, num_objects):
+        self.logger = logger.Logger()
+        self.num_objects = num_objects
+    
+    def run(self):
+        k4a = PyK4A(
+            Config(
+                color_resolution=pyk4a.ColorResolution.RES_1080P,
+                depth_mode=pyk4a.DepthMode.NFOV_UNBINNED,
+                synchronized_images_only=True,
+            )
+        )
+        k4a.start()
+
+        while True:
+            timestamp = time.time()
+            color_image = k4a.get_last_color_image()
+            if color_image is not None:
+                binary_image = cv2.cvtColor(color_image, cv2.COLOR_BGR2GRAY)
+                _, binary_image = cv2.threshold(binary_image, 128, 255, cv2.THRESH_BINARY)
+                objects = pyzbar.decode(binary_image)
+
+                if len(objects) == self.num_objects:
+                    log_line = f"{timestamp}"
+                    for i, obj in enumerate(objects):
+                        log_line += f", {obj.data.decode('utf-8')}, {obj.rect}, {obj.angle}"
+                    self.logger.log(log_line)
+
+                k4a.release_last_color_image()
+
+    def stop(self):
+        self.logger.close()
+
+
 
 
 
@@ -106,3 +222,6 @@ class QRCodeDetector:
 
 
 #QRCodeDetector.detect_qr_codes(num_obj=5)
+detector = QR_Detector_3(num_objects=5)
+detector.stop()
+
