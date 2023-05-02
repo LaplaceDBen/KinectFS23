9d772e2939ef183004f48ba9ffa83be78001251a
RaphaBru <raphaelbrunold@hotmail.com>
2023-05-02 19:39:52 +0200

    modal changes

--- raffi/blender_modal5.py
+++ raffi/blender_modal5.py
@@ -5,7 +5,9 @@ import bpy
 import math
 import threading
 
-logPath = r"C:\Users\rapha\OneDrive\Desktop\CDS_FS23\Projektarbeit\GitHubNew\KinectFS23\raffi\qr_codes_test.log"
+# logPath = r"C:\Users\rapha\OneDrive\Desktop\CDS_FS23\Projektarbeit\GitHubNew\KinectFS23\raffi\qr_codes_test.log"      # Test-Logfile
+logPath = r"C:\Users\rapha\OneDrive\Desktop\CDS_FS23\Projektarbeit\GitHubNew\KinectFS23\qr_codes.log"                   # Echtes Logfile
+
 house_A_object_name = "House"
 house_B_object_name = "Modern House"
 house_C_object_name = "Apartment building"
@@ -59,40 +61,40 @@ class OBJECT_OT_modal_operator(bpy.types.Operator):
                         house_A = (int(obj[1]), int(obj[2]), float(obj[3]))
                         obj1 = bpy.data.objects.get(house_A_object_name)
                         if obj1:
-                            obj1.location.x = house_A[0]*0.04
-                            obj1.location.y = house_A[1]*0.04
+                            obj1.location.x = house_A[0]*0.08
+                            obj1.location.y = house_A[1]*0.08
                             obj1.rotation_euler.z = math.radians(house_A[2])
 
                     elif obj[0] == 'Haus_B':
                         house_B = (int(obj[1]), int(obj[2]), float(obj[3]))
                         obj2 = bpy.data.objects.get(house_B_object_name)
                         if obj2:
-                            obj2.location.x = house_B[0]*0.04
-                            obj2.location.y = house_B[1]*0.04
+                            obj2.location.x = house_B[0]*0.08
+                            obj2.location.y = house_B[1]*0.08
                             obj2.rotation_euler.z = math.radians(house_B[2])
 
                     elif obj[0] == 'Haus_C':
                         house_C = (int(obj[1]), int(obj[2]), float(obj[3]))
                         obj3 = bpy.data.objects.get(house_C_object_name)
                         if obj3:
-                            obj3.location.x = house_C[0]*0.04
-                            obj3.location.y = house_C[1]*0.04
+                            obj3.location.x = house_C[0]*0.08
+                            obj3.location.y = house_C[1]*0.08
                             obj3.rotation_euler.z = math.radians(house_C[2])
 
                     elif obj[0] == 'Haus_D':
                         house_D = (int(obj[1]), int(obj[2]), float(obj[3]))
                         obj4 = bpy.data.objects.get(house_D_object_name)
                         if obj4:
-                            obj4.location.x = house_D[0]*0.04
-                            obj4.location.y = house_D[1]*0.04
+                            obj4.location.x = house_D[0]*0.08
+                            obj4.location.y = house_D[1]*0.08
                             obj4.rotation_euler.z = math.radians(house_D[2])
 
                     elif obj[0] == 'Baum':
                         tree = (int(obj[1]), int(obj[2]), float(obj[3]))
                         obj5 = bpy.data.objects.get(tree_object_name)
                         if obj5:
-                            obj5.location.x = tree[0]*0.04
-                            obj5.location.y = tree[1]*0.04
+                            obj5.location.x = tree[0]*0.08
+                            obj5.location.y = tree[1]*0.08
                             obj5.rotation_euler.z = math.radians(tree[2])
 
         # Call view_layer.update() and wm.redraw_timer() outside the loop
