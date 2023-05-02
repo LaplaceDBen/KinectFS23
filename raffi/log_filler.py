a0a9b111a362dbeafb18356dd8ddf49f7f182858
RaphaBru <raphaelbrunold@hotmail.com>
2023-05-02 17:57:47 +0200

    small changes log_filler

--- raffi/log_filler.py
+++ raffi/log_filler.py
@@ -1,16 +1,35 @@
 import random
 import time
+from datetime import datetime
 
 logPath = r"C:\Users\rapha\OneDrive\Desktop\CDS_FS23\Projektarbeit\GitHubNew\KinectFS23\raffi\qr_codes_test.log"
 
-for i in range(2000):
-    value1 = random.randint(10, 500)
-    value2 = random.randint(10, 500)
-    value3 = random.randint(30, 60)
-    newLine = f"QRCODE: Haus_B, (1,1),1 | QRCODE: Baum, (1569, 677), 87.22 | QRCODE: Haus_A, ({value1}, {value2}), {value3} | QRCODE: Haus_D, (1835, 1246), 87.12 | QRCODE: Haus_C, (1296, 1284), 89.24 | 2023-04-21 19:25:39.979248\n"
-    
+for i in range(300):
+    value1 = random.randint(200, 1800)
+    value2 = random.randint(200, 1800)
+    value3 = random.randint(0, 360)
+    value4 = random.randint(-1800, -200)
+    value5 = random.randint(-1800, -200)
+    value6 = random.randint(0, 360)
+    value7 = random.randint(-1800, -200)
+    value8 = random.randint(200, 1800)
+    value9 = random.randint(0, 360)
+    value10 = random.randint(200, 1800)
+    value11 = random.randint(-1800, -200)
+    value12 = random.randint(0, 360)
+    value13 = random.randint(-1600, 1600)
+    value14 = random.randint(-1600, 1600)
+    value15 = random.randint(0, 360)
+
+
+    # newLine = f"QRCODE: Baum, ({value7}, {value8}), {value9} | QRCODE: Haus_B, ({value4}, {value5}), {value6} | QRCODE: Haus_A, ({value1}, {value2}), {value3} | QRCODE: Haus_C, ({value10}, {value11}), {value12} | QRCODE: Haus_D, ({value13}, {value14}), {value15} | 16:49:57.222173\n"
+    newLine = f"QRCODE: Haus_A, ({value1}, {value2}), {value3} | 16:49:57.222173\n"
+
     with open(logPath, 'a') as f:
         f.write(newLine)
-        
-    time.sleep(0.01)  # wait for 0.1 seconds before next loop iteration
+    
+    # current_time = datetime.now().time()
+    # print(f'Just wrote a line, current time: {current_time}')
+
+    time.sleep(0.1)  # wait for 0.1 seconds before next loop iteration
     
